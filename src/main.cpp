9689a38759baaa210bec3c425dced3440f7bf699
FTS427 <FTS427@outlook.com>
2025-05-04 14:30:16 +0800

    U | format codes

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,20 @@ int main(int argc, char* args[]) {
 
     try {
         RandomLineReader reader(args[1]);
-        printf("Total lines: %zu\n", reader.getTotalLines());
 
         if (argc >= 3) {
             try {
                 size_t count = std::stoull(args[2]);
                 auto random_lines = reader.getRandomLines(count);
-                
+
                 if (random_lines.size() < count) {
                     printf("Warning: Requested %zu lines, but only %zu lines are available.\n", count, random_lines.size());
                 }
                 for (size_t i = 0; i < random_lines.size(); ++i) {
-                    printf("\033[33m%zu\033[0m content:\n%s\n", i+1,random_lines[i].c_str());
+                    printf("\033[33m%zu\033[0m content:\n%s\n", i + 1, random_lines[i].c_str());
                 }
-            } catch (const std::exception& e) {
+            }
+            catch (const std::exception& e) {
                 printf("Error: Invalid number format\n");
                 return 1;
             }
--- src/random_line_reader.cpp
+++ src/random_line_reader.cpp
@@ -1,5 +1,6 @@
 #include "random_line_reader.hpp"
 #include <stdexcept>
+#include <fstream>
 
 std::random_device RandomLineReader::rd_;
 std::mt19937 RandomLineReader::gen_(rd_());
@@ -17,16 +18,16 @@ void RandomLineReader::readFile(const std::string& file_path) {
     file.seekg(0, std::ios::end);
     const auto file_size = file.tellg();
     file.seekg(0, std::ios::beg);
-    
+
     lines_.reserve(static_cast<std::size_t>(file_size) / 30); // predict the number of each line's words is 30
-    
+
     std::string temp_line;
     temp_line.reserve(256);
-    
+
     while (std::getline(file, temp_line)) {
         lines_.push_back(std::move(temp_line));
     }
-    
+
     if (lines_.empty()) {
         throw std::runtime_error("Empty file: " + file_path);
     }
@@ -48,19 +49,19 @@ std::vector<std::string> RandomLineReader::getRandomLines(size_t count) const {
     if (lines_.empty()) {
         throw std::runtime_error("No lines available");
     }
-    
+
     count = std::min(count, lines_.size());
-    
+
     std::vector<size_t> indices(lines_.size());
     std::iota(indices.begin(), indices.end(), 0);
     std::shuffle(indices.begin(), indices.end(), gen_);
-    
+
     std::vector<std::string> result;
     result.reserve(count);
-    
+
     for (size_t i = 0; i < count; ++i) {
         result.push_back(lines_[indices[i]]);
     }
-    
+
     return result;
 }
